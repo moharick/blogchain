04af2ee3a39d6b20c867957faf7d5b8e6542dfed
moharick <moharick@gmail.com>
2019-09-24 15:20:55 +0300

    Add views and forms.

---
+++ app/auth/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,ValidationError
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
+
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField("Your Email Address",validators=[Required(),Email()])
+    username = StringField("Enter your username",validators=[Required()])
+    password = PasswordField("Password",validators=[Required(),EqualTo('password_confirm',message='Passwords must match')])
+    password_confirm = PasswordField('Confirm passwords',validators=[Required()])
+    submit = SubmitField("Sign Up")
+
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError('Email already exists.')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('Username already exists.')
+
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Your Email Address",validators=[Required(),Email()])
+    password = PasswordField("Password",validators=[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField("Sign In")
