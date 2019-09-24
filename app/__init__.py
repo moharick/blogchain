6158d30cace29877adbc9e8703ffddef8b0d4882
moharick <moharick@gmail.com>
2019-09-24 15:22:35 +0300

    Make changes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,47 @@
 from flask import Flask
-from config import Config
-app =  Flask(__name__)
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
+from flask_login import LoginManager
 
-def create_app():
-    app.config.from_object(Config)
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+simple = SimpleMDE()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Creating the app configurations
+
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
+
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+
     return app
---
+++ app/models.py
@@ -0,0 +1,96 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+from datetime import datetime
+from . import login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255), index = True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    pitches = db.relationship('Pitch', backref='user', lazy="dynamic")
+    comments = db.relationship("Comment", backref="user", lazy="dynamic")
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+
+class Pitch(db.Model):
+
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title= db.Column(db.String(300), index=True)
+    content = db.Column(db.String(300), index=True)
+    category = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comments = db.relationship('Comment', backref='pitch', lazy="dynamic")
+
+    date = db.Column(db.String)
+    time = db.Column(db.String)
+
+    def save_pitch(self, pitch):
+        ''' Save the pitches '''
+        db.session.add(pitch)
+        db.session.commit()
+
+    # display pitches
+    @classmethod
+    def get_pitches(id):
+        pitches = Pitch.query.filter_by(category = id).all()
+        return pitches
+
+    def __repr__(self):
+        return f"Pitch('{self.id}', '{self.time}')"
+
+
+
+class Comment(db.Model):
+
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    post_comment = db.Column(db.String(255), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+    date = db.Column(db.String)
+    time = db.Column(db.String)
+
+    def save_comment(self):
+        ''' Save the comments '''
+        db.session.add(self)
+        db.session.commit()
+
+
+
+    # display comments
+    @classmethod
+    def get_comments(cls, id):
+        comments = Comment.query.filter_by(pitch_id=id).all()
+        return comments
