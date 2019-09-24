a804e998d595a30a7e537619f578afb854b5db70
moharick <moharick@gmail.com>
2019-09-24 15:23:02 +0300

    add migrations.

--- config.py
+++ config.py
@@ -1,2 +1,42 @@
+import os
+
 class Config:
-    DEBUG=True
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:zuhura18@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static/images'
+
+
+    SECRET_KEY = '12323344ew'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SENDER_EMAIL = 'moharick@gmail.com'
+
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+        SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+        debug = os.environ.get("DEBUG")
+
+
+class TestConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://thomas:psqltoto29@localhost/pitchperfect_test'
+    pass
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:zuhura18@localhost/blog'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
--- manage.py
+++ manage.py
@@ -1,11 +1,31 @@
+from app import create_app,db
+from flask_script import Manager,Shell,Server
+from flask_migrate import Migrate, MigrateCommand
+from app.models import User,Pitch,Comment
 
-from flask_script import Manager , Server
+# Creating app instance
+app = create_app('development')
 
-from app import create_app
-app=create_app()
+manager = Manager(app)
+migrate = Migrate(app,db)
 
-manager =  Manager(app)
-manager.add_command('runserver',Server(use_debugger=True))
+manager.add_command('server',Server)
+manager.add_command('db',MigrateCommand)
 
-if __name__=="__main__":
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Pitch = Pitch,Comment = Comment)
+
+
+
+if __name__ == '__main__':
     manager.run()
